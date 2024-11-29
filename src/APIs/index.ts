a266e4c972a3f9105d58f57f9fb57c8c5676640b
Muhammad Abdullah <abdulx125@gmail.com>
2024-11-29 17:22:54 +0500

    chore:fix all merge

--- src/APIs/index.ts
+++ src/APIs/index.ts
@@ -1,16 +1,18 @@
-import { Application } from 'express';
-import { API_ROOT } from '../constant/application'
+import { Application } from "express";
+import { API_ROOT } from "../constant/application";
 
-import General from './router'
-import authRoutes from './user/authentication'
-import userManagementRoutes from './user/management'
-import container_router from '../APIs/container/routes';
+import General from "./router";
+import authRoutes from "./user/authentication";
+import userManagementRoutes from "./user/management";
+import container_router from "../APIs/container/routes";
+import employeRouter from '../APIs/employeedata/router/router1'
 
 const App = (app: Application) => {
-    app.use(`${API_ROOT}`, General);
-    app.use(`${API_ROOT}`, authRoutes);
-    app.use(`${API_ROOT}/user`, userManagementRoutes);
-    app.use(`${API_ROOT}/container`, container_router);
-}
+  app.use(`${API_ROOT}`, General);
+  app.use(`${API_ROOT}`, authRoutes);
+  app.use(`${API_ROOT}/user`, userManagementRoutes);
+  app.use(`${API_ROOT}/employe`, employeRouter);
+  app.use(`${API_ROOT}/container`, container_router);
+};
 
-export default App
+export default App;
