c0adae898a1d3c5358a2f43bae4a821a2ab27d2e
Muhammad Abdullah <abdulx125@gmail.com>
2024-12-06 21:35:50 +0500

    fix:linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ export default tseslint.config({
     ...tseslint.configs.recommendedTypeChecked,
     eslintConfigPrettier,
   ],
+
   rules: {
     "no-console": "error",
     "no-useless-catch": 0,
