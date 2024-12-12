415e67fb3b3e0fe133fb967245b66b330cca7602
Muhammad Abdullah <abdulx125@gmail.com>
2024-12-12 22:56:14 +0500

    removed smtp

--- src/utils/emailSender.ts
+++ src/utils/emailSender.ts
@@ -25,33 +25,34 @@
 //   });
 // };
 
-
 import nodemailer from "nodemailer";
+import logger from "../handlers/logger";
 
 export const sendEmail = async (
   recipient: string,
   subject: string,
   text: string,
-  attachments: any[] = [],
-  replyTo?: string
+  attachments: any[] = []
 ) => {
   const transporter = nodemailer.createTransport({
     service: "Gmail",
+    // service: "smtp.gmail.com",
+    port: 465,
     auth: {
       user: "khansuzair1@gmail.com",
       pass: "yena sysp bncd uwvz", // Consider using environment variables for better security
     },
-    secure: true, // Ensures TLS encryption
+    secure: false, // Ensures TLS encryption
   });
 
   // Verify transporter connection
   await new Promise((resolve, reject) => {
     transporter.verify((error, success) => {
       if (error) {
-        console.error("Error verifying transporter:", error);
+        logger.error("Error verifying transporter:", error);
         reject(error);
       } else {
-        console.log("Transporter is ready to send emails");
+        logger.info("Transporter is ready to send emails");
         resolve(success);
       }
     });
@@ -64,17 +65,16 @@ export const sendEmail = async (
     text,
     html: `<p>${text}</p>`, // Include HTML format for rich-text compatibility
     attachments,
-    replyTo: replyTo || "khansuzair1@gmail.com", // Default to sender's email if replyTo is not provided
   };
 
   // Send the email
   await new Promise((resolve, reject) => {
     transporter.sendMail(mailData, (err, info) => {
       if (err) {
-        console.error("Error sending email:", err);
+        logger.error("Error sending email:", err);
         reject(err);
       } else {
-        console.log("Email sent successfully:", info);
+        logger.info("Email sent successfully:", info);
         resolve(info);
       }
     });
